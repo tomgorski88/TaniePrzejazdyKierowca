e5e96a060747377342c1a711c3ed9e9b99eaaa12
Tomasz Gorski <jomal@vp.pl>
2023-06-30 13:04:54 +0200

    Match fragments in main activity

--- TaniePrzejazdyKierowca/MainActivity.cs
+++ TaniePrzejazdyKierowca/MainActivity.cs
@@ -1,4 +1,5 @@
 ﻿using Android.App;
+using Android.Graphics;
 using Android.OS;
 using Android.Runtime;
 using AndroidX.AppCompat.App;
@@ -28,6 +29,8 @@ namespace TaniePrzejazdyKierowca
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            ConnectViews();
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
@@ -39,12 +42,71 @@ namespace TaniePrzejazdyKierowca
         void ConnectViews()
         {
             bnve = (BottomNavigationViewEx)FindViewById(Resource.Id.bnve);
+            bnve.EnableItemShiftingMode(false);
+            bnve.EnableShiftingMode(false);
+            bnve.NavigationItemSelected += Bnve_NavigationItemSelected;
+
+            BnveToActionColor(0);
+
             viewPager = (ViewPager)FindViewById(Resource.Id.viewpager);
 
             viewPager.OffscreenPageLimit = 3;
             viewPager.BeginFakeDrag();
 
             SetupViewPager();
+        }
+
+        private void Bnve_NavigationItemSelected(object sender, Android.Support.Design.Widget.BottomNavigationView.NavigationItemSelectedEventArgs e)
+        {
+            if(e.Item.ItemId == Resource.Id.action_earning)
+            {
+                viewPager.SetCurrentItem(1, true);
+                BnveToActionColor(1);
+            }
+            else if (e.Item.ItemId == Resource.Id.action_home)
+            {
+                viewPager.SetCurrentItem(0, true);
+                BnveToActionColor(0);
+            }
+            else if (e.Item.ItemId == Resource.Id.action_rating)
+            {
+                viewPager.SetCurrentItem(2, true);
+                BnveToActionColor(2);
+            }
+            else if (e.Item.ItemId == Resource.Id.action_account)
+            {
+                viewPager.SetCurrentItem(3, true);
+                BnveToActionColor(3);
+            }
+        }
+
+        void BnveToActionColor(int index)
+        {
+            var img = bnve.GetIconAt(1);
+            var txt = bnve.GetLargeLabelAt(1);
+            img.SetColorFilter(Color.Rgb(255, 255, 255));
+            txt.SetTextColor(Color.Rgb(255, 255, 255));
+
+            var img0 = bnve.GetIconAt(0);
+            var txt0 = bnve.GetLargeLabelAt(0);
+            img.SetColorFilter(Color.Rgb(255, 255, 255));
+            txt.SetTextColor(Color.Rgb(255, 255, 255));
+
+            var img2 = bnve.GetIconAt(2);
+            var txt2 = bnve.GetLargeLabelAt(2);
+            img.SetColorFilter(Color.Rgb(255, 255, 255));
+            txt.SetTextColor(Color.Rgb(255, 255, 255));
+
+            var img3 = bnve.GetIconAt(3);
+            var txt3 = bnve.GetLargeLabelAt(3);
+            img.SetColorFilter(Color.Rgb(255, 255, 255));
+            txt.SetTextColor(Color.Rgb(255, 255, 255));
+
+            var imgIndex = bnve.GetIconAt(index);
+            var textIndex = bnve.GetLargeLabelAt(index);
+
+            imgIndex.SetColorFilter(Color.Rgb(24, 191, 242));
+            textIndex.SetTextColor(Color.Rgb(24, 191, 242));
         }
 
         private void SetupViewPager()
